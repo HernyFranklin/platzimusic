74ea77278a587817a1c9cd2edb841c8ee9755be0
fchoque <fchoque@mc4.com.bo>
2020-05-11 19:16:32 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appname: 'platzimusichf',
+    apikey: '1c6a847cb9be4b8f36829c77650e0acf',
+    secret: '4ffcca3df75870c41c8254255475d81f',
+    registeredTo: 'fchoque19'
+}
+
+
+// Application name	platzimusichf
+// API key	1c6a847cb9be4b8f36829c77650e0acf
+// Shared secret	4ffcca3df75870c41c8254255475d81f
+// Registered to	fchoque19
